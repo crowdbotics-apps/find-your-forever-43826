d5e94cf206936cb4c165a98cba02cc844931c91d
Crowdbotics <team@crowdbotics.com>
2023-10-08 04:17:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "find-your-forever-43826",
+  url: "https://find-your-forever-43826.botics.co",
+  api: "https://find-your-forever-43826.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
